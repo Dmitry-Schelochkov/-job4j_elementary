8f552591a5bf26ccaddb964b1b0b25c5fa3d4826
DS2410 <70708181+DS2410@users.noreply.github.com>
2025-09-03 19:43:55 +0400

    1. Модульные тесты [#505277]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.converter;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    void whenConvert180RblThen2Euro() {
+        float input = 180;
+        float expected = 2;
+        float output = (float) Converter.rubleToEuro(input);
+        float value = 0.0001f;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenConvert3690RblThen41Euro() {
+        float input = 3690;
+        float expected = 41;
+        float output = (float) Converter.rubleToEuro(input);
+        float value = 0.0001f;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenConvert225RblThen3Dollar() {
+        float input = 225;
+        float expected = 3;
+        float output = (float) Converter.rubleToDollar(input);
+        float value = 0.0001f;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenConvert1050RblThen14Dollar() {
+        float input = 1050;
+        float expected = 14;
+        float output = (float) Converter.rubleToDollar(input);
+        float value = 0.0001f;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+}
